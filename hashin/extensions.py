4dd64b342ebf58c485b3482d706773d44805f0a0
Siddhartha Tiwari <201851127@iiitvadodara.ac.in>
2020-07-13 21:50:28 +0530

    add file extensions for different languages

---
+++ hashin/extensions.py
@@ -0,0 +1,6 @@
+file_extensions = {}
+file_extensions['python'] = ".py"
+file_extensions['c++17'] = ".cpp"
+file_extensions['c++11'] = ".cpp"
+file_extensions['java'] = ".java"
+file_extensions['c'] = ".c"
